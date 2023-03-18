fb0ea8d5bfa45a3a9e368be8f0081bde13625308
Grant Birkinbine <grantbirki@github.com>
2023-03-18 23:44:09 +0000

    start employment page

---
+++ src/config/pages/employment/features.js
@@ -0,0 +1,62 @@
+// Configuration for the features component on the homepage
+
+const features = {
+  // If the features component should be enabled or not on the homepage
+  enabled: false,
+
+  // highlight text for the features component
+  highlight: 'Features',
+
+  // Title for the features component
+  title: 'What you get with AstroWind',
+
+  // Subtitle for the features component
+  subtitle:
+    'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.',
+
+  // List of features to display with HTML support
+  items: [
+    [
+      {
+        title: 'Astro + Tailwind CSS Integration',
+        description:
+          'A seamless integration between two great frameworks that offer high productivity, performance and versatility.',
+        icon: 'tabler:brand-tailwind',
+      },
+      {
+        title: 'Ready-to-use Components',
+        description:
+          'Widgets made with Tailwind CSS ready to be used in Marketing Websites, SaaS, Blogs, Personal Profiles, Small Business...',
+        icon: 'tabler:components',
+      },
+      {
+        title: 'Best Practices',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.',
+        icon: 'tabler:list-check',
+      },
+    ],
+    [
+      {
+        title: 'Excellent Page Speed',
+        description:
+          'Having a good page speed impacts organic search ranking, improves user experience (UI/UX) and increase conversion rates.',
+        icon: 'tabler:rocket',
+      },
+      {
+        title: 'Search Engine Optimization (SEO)',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.',
+        icon: 'tabler:arrows-right-left',
+      },
+      {
+        title: 'Open to new ideas and contributions',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.',
+        icon: 'tabler:bulb',
+      },
+    ],
+  ],
+};
+
+export default features;
---
+++ src/config/pages/employment/features2.js
@@ -0,0 +1,75 @@
+// Configuration for the 'features2' section of the home page
+
+const features2 = {
+  // If the features2 section should be displayed
+  enabled: false,
+
+  // The title of the features2 section
+  title: 'Most used widgets',
+
+  // The subtitle of the features2 section
+  subtitle: 'Provides frequently used components for building websites using Tailwind CSS',
+
+  // Small highlight text at the top of the features2 section
+  highlight: 'Components',
+
+  // The items in the features2 section
+  items: [
+    {
+      title: 'Headers',
+      description:
+        'In general, Headers contain information that makes it easier for visitors to interact with the website.',
+      icon: 'flat-color-icons:template',
+    },
+    {
+      title: 'Heros',
+      description:
+        'If you want your website to get more than its fair share of visitors, the Hero section needs to be stellar.',
+      icon: 'flat-color-icons:gallery',
+    },
+    {
+      title: 'Features',
+      description:
+        'Display your product in action and how the Features actually create a solution for your target customer.',
+      icon: 'flat-color-icons:todo-list',
+    },
+    {
+      title: 'Content',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
+      icon: 'flat-color-icons:document',
+    },
+    {
+      title: 'Call-to-Action',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
+      icon: 'flat-color-icons:advertising',
+    },
+    {
+      title: 'Pricing',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
+      icon: 'flat-color-icons:currency-exchange',
+    },
+    {
+      title: 'Testimonial',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
+      icon: 'flat-color-icons:voice-presentation',
+    },
+    {
+      title: 'Contact',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
+      icon: 'flat-color-icons:business-contact',
+    },
+    {
+      title: 'Footers',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
+      icon: 'icon-park:page-template',
+    },
+  ],
+};
+
+export default features2;
