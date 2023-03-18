d86d354052c6423bbeb27ef746f8d41139a5702a
Grant Birkinbine <grantbirki@github.com>
2023-03-18 22:05:58 +0000

    update hero text

--- src/config/home/hero.js
+++ src/config/home/hero.js
@@ -7,24 +7,22 @@ const hero = {
   // Hero title with HTML support
   title: {
     enabled: true, // If the title should be enabled or not
-    text: `Free template for<span class="text-accent dark:text-white highlight"> Astro 2.0</span> + Tailwind CSS + GitHub Pages`,
+    text: `Eltana`,
   },
 
   // Hero subtitle with HTML support
   subtitle: {
     enabled: true, // If the subtitle should be enabled or not
     text: `<span class="hidden sm:inline">
-    <span class="font-semibold">AstroWind</span> is a free, customizable and production-ready template for Astro 2.0
-    + Tailwind CSS.</span
+    <span class="font-semibold">Eltana</span> is a wood-fired bagel cafe located in Seattle, WA.<br></span
     >
-    <span class="block mb-1 sm:hidden font-bold text-blue-600">AstroWind: Production-ready.</span> Suitable for Startups,
-    Small Business, Sass Websites, Professional Portfolios, Marketing Websites, Landing Pages & Blogs.`,
+    <span class="block mb-1 sm:hidden font-bold text-yellow-700">Wood-Fired Bagel Cafe</span>Bagels, Coffee, and more!`,
   },
 
   // First CTA block
   callToAction: {
     // Enable or disable first CTA
-    enabled: true,
+    enabled: false,
     // CTA text
     text: 'Get template',
     // CTA link
@@ -40,7 +38,7 @@ const hero = {
   // Second CTA block (optional)
   callToAction2: {
     // Enable or disable second CTA
-    enabled: true,
+    enabled: false,
     // CTA text
     text: 'Learn more',
     // CTA link
