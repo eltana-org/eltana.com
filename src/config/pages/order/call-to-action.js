6b558d2b6ddc4e452151d6fb7432e2a14140e716
Grant Birkinbine <grantbirki@github.com>
2023-03-19 18:34:01 +0000

    add content to /order page

---
+++ src/config/pages/order/call-to-action.js
@@ -0,0 +1,35 @@
+// Configuration for the site wide call to action
+// This component config can be used on any page to display an identical call to action
+
+// ----------- imports -----------
+import { getPermalink } from '~/utils/permalinks';
+// ----------- imports -----------
+
+const callToAction = {
+  // If the call to action should be displayed
+  enabled: true,
+
+  // The title of the call to action component
+  title: 'Looking for Same Day Delivery?',
+
+  // The subtitle of the call to action component (supports HTML)
+  subtitle: `Checkout our same day delivery options through Postmates and Uber Eats.`,
+
+  // Button configuration for the call to action component
+  button: {
+    // If the button should be displayed
+    enabled: true,
+    // The text of the button
+    text: 'Order Now',
+    // The link of the button
+    href: getPermalink('/same-day-delivery'),
+    // The icon to the left of the button text
+    icon: 'tabler:bolt',
+    // HTML target attribute
+    target: '_self',
+    // HTML rel attribute
+    rel: [],
+  },
+};
+
+export default callToAction;
