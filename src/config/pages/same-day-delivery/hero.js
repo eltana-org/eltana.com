5511fb5b968f3b1f3391d4c4efd0c83fcbfb0cdd
grantbirki <grantbirki@github.com>
2023-03-28 22:43:50 +0100

    use spread-grid img

--- src/config/pages/same-day-delivery/hero.js
+++ src/config/pages/same-day-delivery/hero.js
@@ -59,7 +59,7 @@ const hero = {
     // If the image should be enabled or not
     enabled: true,
     // Image source
-    src: import('~/assets/images/paper-bag.jpg'),
+    src: import('~/assets/images/spread-grid.jpg'),
     // Image alt text
     alt: 'Eltana bagels for delivery and paper bag',
   },
