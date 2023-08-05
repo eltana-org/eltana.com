2b157e657f64db732fb32869bbe63d0996a8dc4a
grantbirki <grantbirki@github.com>
2023-08-05 14:56:39 +0100

    https://github.com/GrantBirki/astrowind/pull/199

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,9 +33,6 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [readingTimeRemarkPlugin],
   },
-  experimental: {
-    assets: true,
-  },
   integrations: [
     icon({
       include: {
